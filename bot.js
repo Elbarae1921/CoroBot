ff9dead2c8fac544d9b0d7534c879bc7b14b9581
Elbarae Rguig <elbarae1921@gmail.com>
2020-03-17 18:54:51 +0100

    Fixed an error from the last commit

--- bot.js
+++ bot.js
@@ -146,13 +146,13 @@ client.on("message", async message => {
 
                 if (countries.includes(args[1])) {
                     const data = await Corona.getCountryDataArray(args[1]);
-                    var cases = data[0] == "" ? "" : `> ${data[0]}`;
-                    cases += data[1] == "" ? "" : ` (${data[1]})`;
-                    var deaths = data[2] == "" ? "" : `> ${data[2]}`;
-                    deaths += data[3] == "" ? "" : ` (${data[3]})`;
-                    var recovered = data[4] == "" ? "" : `> ${data[4]}`;
-                    var active = data[5] == "" ? "" : `> ${data[5]}`;
-                    var serious = data[6] == "" ? "" : `> ${data[6]}`;
+                    var cases = data[0].replace(/ /g, "") == "" ? "" : `> ${data[0]}`;
+                    cases += data[1].replace(/ /g, "") == "" ? "" : ` (${data[1]})`;
+                    var deaths = data[2].replace(/ /g, "") == "" ? "" : `> ${data[2]}`;
+                    deaths += data[3].replace(/ /g, "") == "" ? "" : ` (${data[3]})`;
+                    var recovered = data[4].replace(/ /g, "") == "" ? "" : `> ${data[4]}`;
+                    var active = data[5].replace(/ /g, "") == "" ? "" : `> ${data[5]}`;
+                    var serious = data[6].replace(/ /g, "") == "" ? "" : `> ${data[6]}`;
                     const reply = new MessageEmbed()
                         .setColor(message.guild.me.displayHexColor)
                         .setTitle(`Coronavirus stats in ${args[1]}`)
