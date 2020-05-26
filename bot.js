4c380a51529ed3b5c991c9cb95ce7265f700b135
Elbarae <elbarae1921@gmail.com>
2020-05-26 17:12:44 +0100

    Updated ?coron manual

--- bot.js
+++ bot.js
@@ -340,7 +340,7 @@ client.on("message", async message => {
                 .setTitle("Coronavirus command")
                 .setTimestamp()
                 .setFooter(client.user.username, client.user.displayAvatarURL)
-                .addFields([{ name: "?corona", value: "shows help on how to use the command" }, { name: "?corona whatis", value: "what is coronavirus?" }, { name: "?corona symptoms", value: "a list of possible symptoms" }, { name: "?corona prevent", value: "how to prevent the covid-19" }, { name: "?corona cure", value: "news about the covid-19 cure" }, { name: "?corona stats", value: "covid-19 statistics (cases/deaths/recoveries) worldwide" }, { name: "?corona stats [country]", value: "detailed statistics on the covid-19 spreading in the given country" }, { name: "?corona api", value: "Get url of the api used by the bot" }]);
+                .addFields([{ name: "?corona", value: "shows help on how to use the command" }, { name: "?corona whatis", value: "what is coronavirus?" }, { name: "?corona symptoms", value: "a list of possible symptoms" }, { name: "?corona prevent", value: "how to prevent the covid-19" }, { name: "?corona cure", value: "news about the covid-19 cure" }, { name: "?corona stats", value: "covid-19 statistics (cases/deaths/recoveries) worldwide" }, { name: "?corona stats [country]", value: "detailed statistics on the covid-19 spreading in the given country" }, { name: "?corona api", value: "Get url of the api used by the bot" }, { name: "?corona chart", value: "Get an image that contains a chart representing evolution of the number of cases/deaths in the world." }]);
             message.channel.send(reply);
         }
 
