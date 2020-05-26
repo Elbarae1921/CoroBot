12e43ba6c14d5a1f28432676ef2369433d5ef2ce
Elbarae <elbarae1921@gmail.com>
2020-05-26 15:39:05 +0100

    Switched to using my own API for daily data since it supports it now

--- getGraph.js
+++ getGraph.js
@@ -2,23 +2,23 @@ const fs = require('fs');
 const axios = require("axios");
 const chartExporter = require("highcharts-export-server");
 
-const API = "https://covid19.mathdro.id/api/daily";
+const API = "http://api19covid.herokuapp.com/daily";
 
 const CasesGraph = async (oldMessage, channel, MessageAttachment) => {
     console.log("scraping data...");
     const response = await axios.get(API);
     console.log("data scraped, now parsing...");
-    const rawData = response.data;
+    const rawData = response.data.dailyData;
     const casesData = rawData.map(daily => {
         return {
-            y: daily.confirmed.total,
-            x: new Date(daily.reportDate).getTime()
+            y: daily.confirmed,
+            x: daily.date
         }
     });
     const deathsData = rawData.map(daily => {
         return {
-            y: daily.deaths.total,
-            x: new Date(daily.reportDate).getTime()
+            y: daily.deaths,
+            x: daily.date
         }
     });
     console.log("creating chart...");
@@ -87,7 +87,9 @@ const CasesGraph = async (oldMessage, channel, MessageAttachment) => {
         if(oldMessage.deletable) oldMessage.delete();
 
         const att = new MessageAttachment(Buffer(res.data, 'base64'));
+        console.log("chart created now sending...");
         channel.send("", att);
+        console.log("chart sent");
 
         // fs.writeFile("code.txt", res.data, err => {
         //     if(err) console.log(err);
